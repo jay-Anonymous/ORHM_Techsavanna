5849ed7a19f34a427f4781b2949bc7467a305eae
Jay_Anonymous <gnjakai@gmail.com>
2022-09-23 15:06:44 +0300

    For Techsavanna

---
+++ symfony/apps/orangehrm/modules/auth/templates/loginSuccess.php
@@ -0,0 +1,303 @@
+<?php
+$imagePath = theme_path("images/login");
+?>
+<style type="text/css">
+
+    body {
+        background-color: #FFFFFF;
+        height: 700px;
+    }
+
+    img {
+        border: none;
+    }
+    #btnLogin {
+        padding: 0;
+    }
+    input:not([type="image"]) {
+        background-color: transparent;
+        border: none;
+    }
+
+    input:focus, select:focus, textarea:focus {
+        background-color: transparent;
+        border: none;
+    }
+
+    .textInputContainer {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 11px;
+        color: #666666;
+    }
+
+    #divLogin {
+        background: transparent url(<?php echo "{$imagePath}/login.png"; ?>) no-repeat center top;
+        height: 520px;
+        width: 100%;
+        border-style: hidden;
+        margin: auto;
+        padding-left: 10px;
+    }
+
+    #divUsername {
+        padding-top: 153px;
+        padding-left: 50%;
+    }
+
+    #divPassword {
+        padding-top: 35px;
+        padding-left: 50%;
+    }
+
+    #txtUsername {
+        width: 240px;
+        border: 0px;
+        background-color: transparent;
+    }
+
+    #txtPassword {
+        width: 240px;
+        border: 0px;
+        background-color: transparent;
+    }
+
+    #txtUsername, #txtPassword {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 11px;
+        color: #666666;
+        vertical-align: middle;
+        padding-top:0;
+    }
+    
+    #divLoginHelpLink {
+        width: 270px;
+        background-color: transparent;
+        height: 20px;
+        margin-top: 12px;
+        margin-right: 0px;
+        margin-bottom: 0px;
+        margin-left: 50%;
+    }
+
+    #divLoginButton {
+        padding-top: 2px;
+        padding-left: 49.3%;
+        float: left;
+        width: 350px;
+    }
+
+    #btnLogin {
+        background: url(<?php echo "{$imagePath}/Login_button.png"; ?>) no-repeat;
+        cursor:pointer;
+        width: 94px;
+        height: 26px;
+        border: none;
+        color:#FFFFFF;
+        font-weight: bold;
+        font-size: 13px;
+    }
+
+    #divLink {
+        padding-left: 230px;
+        padding-top: 105px;
+        float: left;
+    }
+
+    #divLogo {
+        padding-left: 30%;
+        padding-top: 70px;
+    }
+
+    #spanMessage {
+        background: transparent url(<?php echo "{$imagePath}/mark.png"; ?>) no-repeat;
+        padding-left: 18px; 
+        padding-top: 0px;
+        color: #DD7700;
+        font-weight: bold;
+    }
+    
+    #logInPanelHeading{
+        position:absolute;
+        padding-top:100px;
+        padding-left:49.5%;
+        font-family:sans-serif;
+        font-size: 15px;
+        color: #544B3C;
+        font-weight: bold;
+    }
+    
+    .form-hint {
+    color: #878787;
+    padding: 4px 8px;
+    position: relative;
+    left:-254px;
+}
+
+    
+</style>
+
+<div id="divLogin">
+    <div id="divLogo">
+        <!--<img src="<?php echo "{$imagePath}/logo.png"; ?>" />-->
+        <br><br><br><br><br>
+    </div>
+
+    <form id="frmLogin" method="post" action="<?php echo url_for('auth/validateCredentials'); ?>">
+        <input type="hidden" name="actionID"/>
+        <input type="hidden" name="hdnUserTimeZoneOffset" id="hdnUserTimeZoneOffset" value="0" />
+        <?php 
+            echo $form->renderHiddenFields(); // rendering csrf_token 
+        ?>
+        <div id="logInPanelHeading"><?php $companies=array("shiloah"=>"ShiloahMega","katangi"=>"DIMITZI LTD","grandways"=>"Grandways Ventures Ltd","fulchand"=>"FULCHAND RASHI LTD");
+        //echo __('LOGIN Panel-SHILOAHMEGA'); ?>
+            <select id="company" name="company" style="width:content-box">
+                <option selected="selected" value="">Select Company</option>
+                <?php
+                foreach ($companies as $key=>$value) { ?>
+                <option value="<?=$key?>"><?=$value?></option>    
+              <?php  }
+                ?>
+            </select>
+        </div>
+
+        <div id="divUsername" class="textInputContainer">
+            <?php echo $form['Username']->render(); ?>
+          <span class="form-hint" ><?php echo __('Username'); ?></span> 
+        </div>
+        <div id="divPassword" class="textInputContainer">
+            <?php echo $form['Password']->render(); ?>
+         <span class="form-hint" ><?php echo __('Password'); ?></span>
+         <br>      <br>      <br>
+         <select id="payrollmonth" name="payrollmonth" style="width:140px" >
+             <option value="<?=date("m")?>" selected="selected"><?=date("m")?></option>      
+                             <?php
+                              $months= array("01"=>"01","02"=>"02","03"=>"03","04"=>"04","05"=>"05","06"=>"06","07"=>"07","08"=>"08","09"=>"09","10"=>"10","11"=>"11","12"=>"12");
+                           foreach ($months as $month) {
+                            
+                                
+                                 echo '<option value="'.$month.'">'.$month.'</option>';
+                                 $month++;
+                             }
+                             
+                             
+                             ?>
+                          </select>
+         <select id="payrollyear" name="payrollyear" style="width:140px" >
+                  <option value="<?=date("Y")?>" selected="selected"> <?=date("Y")?></option>  
+                             <?php
+                           $year=2000; $i=2030;
+                             while ($year<=$i) {
+                            
+                                
+                                 echo '<option value="'.$year.'">'.$year.'</option>';
+                                 $year++;
+                             }
+                             
+                             
+                             ?>
+                          </select>
+        </div>
+        <div id="payrollmonth" class="textInputContainer">
+             
+            
+        </div>
+        <div id="divLoginHelpLink"><?php
+            include_component('core', 'ohrmPluginPannel', array(
+                'location' => 'login-page-help-link',
+            ));
+            ?></div>
+        <div id="divLoginButton">
+            <input type="submit" name="Submit" class="button" id="btnLogin" value="<?php echo __('LOGIN'); ?>" />
+            <?php if (!empty($message)) : ?>
+            <span id="spanMessage"><?php echo __($message); ?></span>
+            <?php endif; ?>
+        </div>
+    </form>
+
+</div>
+
+<div style="text-align: center">
+    <?php include_component('core', 'ohrmPluginPannel', array(
+                'location' => 'other-login-mechanisms',
+            )); ?>
+</div>
+
+<?php include_partial('global/footer_copyright_social_links'); ?>
+
+<script type="text/javascript">
+    
+    function calculateUserTimeZoneOffset() {
+        var myDate = new Date();
+        var offset = (-1) * myDate.getTimezoneOffset() / 60;
+        return offset;
+    }
+            
+    function addHint(inputObject, hintImageURL) {
+        if (inputObject.val() == '') {
+            inputObject.css('background', "url('" + hintImageURL + "') no-repeat 10px 3px");
+        }
+    }
+            
+    function removeHint() {
+       $('.form-hint').css('display', 'none');
+    }
+    
+    function showMessage(message) {
+        if ($('#spanMessage').size() == 0) {
+            $('<span id="spanMessage"></span>').insertAfter('#btnLogin');
+        }
+
+        $('#spanMessage').html(message);
+    }
+    
+    function validateLogin() {
+        var isEmptyPasswordAllowed = false;
+        
+        if ($('#txtUsername').val() == '') {
+            showMessage('<?php echo __('Username cannot be empty'); ?>');
+            return false;
+        }
+        
+        if (!isEmptyPasswordAllowed) {
+            if ($('#txtPassword').val() == '') {
+                showMessage('<?php echo __('Password cannot be empty'); ?>');
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    $(document).ready(function() {
+        /*Set a delay to compatible with chrome browser*/
+        setTimeout(checkSavedUsernames,100);
+        
+        $('#txtUsername').focus(function() {
+            removeHint();
+        });
+        
+        $('#txtPassword').focus(function() {
+             removeHint();
+        });
+        
+        $('.form-hint').click(function(){
+            removeHint();
+            $('#txtUsername').focus();
+        });
+        
+        $('#hdnUserTimeZoneOffset').val(calculateUserTimeZoneOffset().toString());
+        
+        $('#frmLogin').submit(validateLogin);
+        
+    });
+
+    function checkSavedUsernames(){
+        if ($('#txtUsername').val() != '') {
+            removeHint();
+        }
+    }
+
+    if (window.top.location.href != location.href) {
+        window.top.location.href = location.href;
+    }
+</script>
---
+++ symfony/apps/orangehrm/templates/_copyright.php
@@ -0,0 +1,8 @@
+<?php 
+$version = '3.3';
+$prodName = 'ShiloahMegaHRM';
+$copyrightYear = date('Y');
+
+?>
+<?php// echo $prodName . ' ' . $version;?><br/>
+&copy;  <?php echo $copyrightYear;?> <a href="http://www.techsavanna.technology" target="_blank"><?php echo $prodName ?></a>. All rights reserved.
